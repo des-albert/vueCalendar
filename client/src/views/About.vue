311e97221a05d1a3b4dc6b94a086f0ed97d67928
Des Albert <dalbert@cray.com>
2019-10-01 16:23:24 -0700

    add Version number

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h2>Sales Operations Calendar</h2>
-    <p>Vue  version - October 1, 2019</p>
+    <p>Vue  version 1.0 - October 1, 2019</p>
     <p>Author : Des Albert</p>
     <h4>Please login with your SalesForce username, Security token, Password</h4>
     <p></p>
